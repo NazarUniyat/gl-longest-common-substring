017ba96e94d946e773223b8181ab811aa36023e5
Nazar <nazaaar50@gmail.com>
2019-02-20 14:47:20 +0200

    test method givenTwoStringsWithCommonSubstr_whenLongestCommonSubstr_returnLongestSubstr

---
+++ src/test/java/SubstringTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SubstringTest {
+
+    @Test
+    public void givenTwoStringsWithCommonSubstr_whenLongestCommonSubstr_returnLongestSubstr(){
+        Substring.longestCommonSubstr();
+        assertEquals();
+    }
+
+}
